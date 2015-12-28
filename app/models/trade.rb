9b8b34c05460c54258ed860f4e2d2ae16bb8bf92
Marco Carvalho <marco@mcorp.io>
2015-12-28 00:08:13 +0000

    last_tid scope and spec

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,4 +2,7 @@ class Trade < ActiveRecord::Base
   enum kind: [:buy, :sell]
 
   validates :tid, uniqueness: true
+
+  scope :tid_order, -> { order(:tid) }
+  scope :last_tid, -> { tid_order.last }
 end
--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Trade, type: :model do
   it { is_expected.to validate_uniqueness_of(:tid) }
   it { is_expected.to define_enum_for(:kind).with([:buy, :sell]) }
+
+  let!(:list) { create_list(:trade, 2) }
+
+  it '#last_tid' do
+    expect(described_class.last_tid).to eq(list.last)
+  end
 end
