7cc780e68f3989b9a99d08c703a4d9f267225bb0
Marco Carvalho <marco@mcorp.io>
2015-12-29 23:52:37 +0000

    #last_valid and spec

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -2,4 +2,16 @@ class ApiToken < ActiveRecord::Base
   belongs_to :user
 
   validates :token, uniqueness: true
+
+  scope :valid, -> do
+    order(:valid_until).where('valid_until > ?', Time.current)
+  end
+
+  def self.last_valid(user)
+    valid.last
+  end
+
+  def self.create_ephemeral_token(user)
+    create(user: user, token: SecureRandom.uuid, valid_until: 1.day.since)
+  end
 end
--- spec/models/api_token_spec.rb
+++ spec/models/api_token_spec.rb
@@ -3,4 +3,20 @@ require 'rails_helper'
 RSpec.describe ApiToken, type: :model do
   it { is_expected.to validate_uniqueness_of :token }
   it { is_expected.to belong_to :user }
+
+  describe '#last_valid' do
+    let!(:user) { create(:user) }
+    
+    context 'has a valid token' do
+      let!(:token1) { create(:api_token, user: user, valid_until: 1.day.since) }
+      let!(:token2) { create(:api_token, user: user, valid_until: 1.minute.since) }
+      let!(:token3) { create(:api_token, user: user, valid_until: 1.second.ago) }
+      it { expect(ApiToken.last_valid(user)).to eq(token1) }
+    end
+
+    context 'has no valid token' do
+      let!(:token1) { create(:api_token, user: user, valid_until: 1.second.ago) }
+      it { expect(described_class.last_valid(user)).to eq(nil) }
+    end
+  end
 end
