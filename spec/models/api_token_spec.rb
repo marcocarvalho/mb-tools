0eec3be6dc98e2dce7b3ce562cdb011618016ff0
Marco Carvalho <marco@mcorp.io>
2015-12-29 18:48:37 +0000

    uniqueness test for token

--- spec/models/api_token_spec.rb
+++ spec/models/api_token_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ApiToken, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_uniqueness_of :token }
 end
