8183597ff0e55f34e3737410c8bd617bfbbd2c2d
Marco Carvalho <marco@mcorp.io>
2015-12-30 20:59:16 +0000

    #last_valid_token and #create_token specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :api_tokens
 
   validates :email, :password, presence: true
   validates :email, uniqueness: true
+
+  def last_valid_token
+    ApiToken.last_valid(self)
+  end
+
+  def create_token
+    ApiToken.create_ephemeral_token(self)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe User, type: :model do
   end
 
   it { is_expected.to have_many :api_tokens }
+
+  context 'save password encripted' do
+    it 'encrypt' do
+      u = User.create(email: 'email@email.com', password: '1234')
+      expect(u.password_digest).not_to eq('1234')
+    end
+  end
+
+  it '#last_valid_token' do
+    expect(ApiToken).to receive(:last_valid).with(subject).and_return(:token)
+    expect(subject.last_valid_token).to eq(:token)
+  end
+
+  it '#create_ephemeral_token' do
+    expect(ApiToken).to receive(:create_ephemeral_token).with(subject).and_return(:token)
+    expect(subject.create_token).to eq(:token)
+  end
 end
