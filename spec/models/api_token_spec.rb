2a14aa3e2fb4aea87ff82d2bdbbc34791b5a3f1e
Marco Carvalho <marco@mcorp.io>
2015-12-30 12:25:13 +0000

    #create_ephemeral_token spec

--- spec/models/api_token_spec.rb
+++ spec/models/api_token_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ApiToken, type: :model do
+  let!(:user) { create(:user) }
   it { is_expected.to validate_uniqueness_of :token }
   it { is_expected.to belong_to :user }
 
   describe '#last_valid' do
-    let!(:user) { create(:user) }
-    
     context 'has a valid token' do
       let!(:token1) { create(:api_token, user: user, valid_until: 1.day.since) }
       let!(:token2) { create(:api_token, user: user, valid_until: 1.minute.since) }
@@ -20,11 +19,14 @@ RSpec.describe ApiToken, type: :model do
     end
   end
 
-  context '#destroy_invalid' do
-    let!(:user) { create(:user) }
+  describe '#destroy_invalid' do
     let!(:token1) { create(:api_token, user: user, valid_until: 1.day.since) }
     let!(:token2) { create(:api_token, user: user, valid_until: 1.minute.since) }
     let!(:token3) { create(:api_token, user: user, valid_until: 1.second.ago) }
     it { expect { ApiToken.destroy_invalid }.to change { ApiToken.count }.by(-1) }
+  end
+
+  describe '#create_ephemeral_token' do
+    it { expect { ApiToken.create_ephemeral_token(user) }.to change { user.api_tokens.count }.by(1) }
   end
 end
